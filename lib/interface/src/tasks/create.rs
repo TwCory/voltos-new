dbe3f8ccd984165db84dce0d0a78a62ba777b8df
TwCory <voltzbigtime@gmail.com>
2025-08-13 13:42:28 -0400

    another update

--- lib/interface/src/tasks/create.rs
+++ lib/interface/src/tasks/create.rs
@@ -5,3 +5,23 @@
 //!	File:           srctree/lib/interface/src/tasks/create.rs
 //!
 //!	Description:    Interface Creation Functions
+
+use crate::common::*;
+use crate::control::{IfControlMgr, Result, IfControlError};
+
+use rtnetlink::packet::rtnl::link::nlas::Nla;
+use rtnetlink::packet::rtnl::link::nlas::InfoKind;
+use rtnetlink::packet::rtnl::link::nlas::InfoData;
+
+impl IfControlMgr {
+    pub async fn create_dummy(&self, name: &str) -> Result<IfKey> {
+        let req = self.nl_handle.link().add();
+        req.message_mut().nlas.push(Nla::IfName(name.to_string()));
+        req.message_mut().nlas.push(Nla::Info(vec![
+            rtnetlink::packet::rtnl::link::nlas::Info::Kind(InfoKind::Dummy),
+        ]));
+        req.execute().await.map_err(|e| IfControlError::Netlink(e.to_string()))?;
+        self.sync().await?;
+        self.id_with_name(name).await
+    }
+}
