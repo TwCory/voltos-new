da384781466a0fc6b4162df9446b221d42d0da19
Cory <70068963+TwCory@users.noreply.github.com>
2025-09-06 15:03:27 -0400

    Delete lib directory

--- lib/interface/src/linux/tunnel.rs
+++
@@ -1,7 +0,0 @@
-// Copyright (C) 2025 Cory Voltz <voltzc@twchl.net>
-
-//!	VoltOS - Network Interface Library
-//!
-//!	File:           srctree/lib/interface/src/linux/tunnel.rs
-//!
-//!	Description:    Management Functions for the Linux TUN/TAP Device Driver
